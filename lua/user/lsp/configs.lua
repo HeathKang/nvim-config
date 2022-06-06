ab0d2dd91527941f3de8df2be7185c8e4d293c2a
heathkang <heath.kang@foxmail.com>
2022-06-06 23:49:41 +0800

    update:
    1. update python lsp to pylsp

--- lua/user/lsp/configs.lua
+++ lua/user/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = { "jsonls", "sumneko_lua","markdown", "pyright"}
+local servers = { "jsonls", "sumneko_lua","markdown", "pylsp"}
 
 lsp_installer.setup {
 	ensure_installed = servers
