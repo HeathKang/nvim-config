e40df7657ea6fa1d1747b9bf4cb19a5037631cf8
Heath Kang <heath.kang@foxmail.com>
2022-10-15 22:59:06 +0800

    feat:
    1. add tssserver lsp;
    2. update telescpe function;
    3. update which keys;

--- lua/configs/lsp/configs.lua
+++ lua/configs/lsp/configs.lua
@@ -5,7 +5,7 @@ end
 
 local lspconfig = require("lspconfig")
 
-local servers = {"jsonls", "sumneko_lua","markdown", "pyright", "hls", "yamlls", "gopls"}
+local servers = {"jsonls", "sumneko_lua","markdown", "pyright", "hls", "yamlls", "gopls", "tsserver"}
 
 lsp_installer.setup {
 	ensure_installed = servers
